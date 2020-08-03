c438dce202f93dfdfcf03ca626dd72b26588d921
Thomaswbt <55548313+Thomaswbt@users.noreply.github.com>
2020-08-03 23:06:42 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,38 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox-icon.jpg');
+    }else{
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('请输入你的名字。')
+    if(myName === null || !myName){
+        setUserName();
+    }else{
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem('name');
+    if(storedName === null || !storedName){
+        setUserName();
+    }else
+        myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+
